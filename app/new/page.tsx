80b03d4cd866e61ed84409f83f9d0f0ef64f36a1
sean <seancaetanomartin@gmail.com>
2024-06-27 17:55:28 +0100

    Adds host view
    Implements basic usage of the AgoraIO SDK (connect, toggle video and
    audio)
    Adds react-icons package, and use the icons for mute, video and stream
    controls.

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -10,10 +10,9 @@ import PrimaryButton from "@/components/primary-button";
 import slugify from "@/lib/slugify";
 
 export default function NewStreamPage() {
-  // TODO: Allow user to forget their stored details
   const [appId, setAppId] = useLocalStorage("app-id", "");
   const [token, setToken] = useLocalStorage("token", "");
-  const [title, setTitle] = useLocalStorage("title", "");
+  const [channel, setChannel] = useLocalStorage("channel", "");
   const [error, setError] = useState("");
   const router = useRouter();
 
@@ -23,41 +22,41 @@ export default function NewStreamPage() {
 
       if (name === "app-id") setAppId(e.target.value);
       if (name === "token") setToken(e.target.value);
-      if (name === "title") setTitle(e.target.value);
+      if (name === "channel") setChannel(e.target.value);
 
       // Clear the error if it exists after a new change
       if (error) setError("");
     },
-    [error, setAppId, setToken, setTitle],
+    [error, setAppId, setToken, setChannel],
   );
 
   const handleNextClick = useCallback(() => {
-    if (!appId || !token || !title) setError("All fields are required.");
+    if (!appId || !token || !channel) setError("All fields are required.");
     else {
-      const slug = slugify(title);
+      const slug = slugify(channel);
       router.push("/" + slug);
     }
-  }, [appId, token, title, router]);
+  }, [appId, token, channel, router]);
 
   return (
     <div className="flex flex-col items-center gap-8">
       <h3
         className={`text-2xl font-bold p-16 text-rose-300 ${pacifico.className}`}
       >
-        Please fill in the details bellow
+        Please fill in the details bellow,
       </h3>
+      <p>
+        All this information can be acquired from the{" "}
+        <Link
+          className="text-rose-600 active:text-rose-300 visited:text-rose-800"
+          target="_blank"
+          href="https://console.agora.io/v2/project-management"
+        >
+          AgoraIO Console
+        </Link>
+      </p>
       <div className="w-1/2 flex flex-col gap-4">
-        <label>
-          Tell us your App ID from AgoraIO (get one{" "}
-          <Link
-            className="text-rose-600 active:text-rose-300 visited:text-rose-800"
-            target="_blank"
-            href="https://console.agora.io/v2/project-management"
-          >
-            here
-          </Link>
-          )
-        </label>
+        <label>Tell us your App ID from AgoraIO</label>
         <input
           className="text-stone-200 bg-stone-800 rounded-xl p-4 w-full placeholder:text-stone-700"
           name="app-id"
@@ -81,12 +80,12 @@ export default function NewStreamPage() {
       </div>
 
       <div className="w-1/2 flex flex-col gap-4">
-        <label>What is your steam title?</label>
+        <label>Tell us your AgoraIO channel name</label>
         <input
           className="text-stone-200 bg-stone-800 rounded-xl p-4 w-full placeholder:text-stone-700"
-          name="title"
+          name="channel"
           type="text"
-          value={title}
+          value={channel}
           onChange={handleChange}
           placeholder="Your token here..."
         />
--- components/agora-host-video.tsx
+++ components/agora-host-video.tsx
@@ -2,14 +2,24 @@
 
 import {
   LocalUser,
+  RemoteUser,
+  useIsConnected,
   useJoin,
   useLocalCameraTrack,
   useLocalMicrophoneTrack,
   usePublish,
+  useRemoteUsers,
 } from "agora-rtc-react";
 import { useCallback, useState } from "react";
+import { AiOutlineAudio as UnmuteIcon } from "react-icons/ai";
+import { AiOutlineAudioMuted as MuteIcon } from "react-icons/ai";
+import { CiStreamOn as StreamOnIcon } from "react-icons/ci";
+import { CiStreamOff as StreamOffIcon } from "react-icons/ci";
+import { IoVideocamOutline as VideoOnIcon } from "react-icons/io5";
+import { IoVideocamOffOutline as VideoOffIcon } from "react-icons/io5";
 
 import PrimaryButton from "./primary-button";
+import SecondaryButton from "./secondary-button";
 
 export default function AgoraHost({
   config: { appId, channel, token },
@@ -18,20 +28,33 @@ export default function AgoraHost({
 }) {
   const [callStarted, setCallStarted] = useState(false);
   const [videoStarted, setVideoStarted] = useState(false);
-  const [muted, setMuted] = useState(false);
+  const [audioStarted, setAudioStarted] = useState(false);
 
-  const { localMicrophoneTrack } = useLocalMicrophoneTrack(!muted);
+  const { localMicrophoneTrack } = useLocalMicrophoneTrack(audioStarted);
   const { localCameraTrack } = useLocalCameraTrack(videoStarted);
 
-  const handleCall = useCallback(() => {
+  const isConnected = useIsConnected();
+
+  const handleStarCall = useCallback(() => {
     setCallStarted(true);
-    setVideoStarted(true);
   }, []);
+
+  const handleEndCall = useCallback(() => {
+    setCallStarted(false);
+  }, []);
+
+  const handleToggleAudio = useCallback(() => {
+    setAudioStarted(!audioStarted);
+  }, [audioStarted]);
+
+  const handleToggleVideo = useCallback(() => {
+    setVideoStarted(!videoStarted);
+  }, [videoStarted]);
 
   useJoin(
     {
       appid: appId,
-      channel: "stream-together",
+      channel,
       token,
     },
     callStarted,
@@ -39,22 +62,82 @@ export default function AgoraHost({
 
   usePublish([localMicrophoneTrack, localCameraTrack]);
 
+  const otherHosts = useRemoteUsers();
+
   return (
-    <div className="flex flex-col gap-8">
-      <div style={{ width: 640, height: 480 }}>
+    <div className="flex flex-col gap-8 w-full items-center">
+      <div className="rounded-3xl overflow-hidden bg-black w-1/2 h-80">
         <LocalUser
           audioTrack={localMicrophoneTrack}
           videoTrack={localCameraTrack}
           cameraOn={videoStarted}
-          micOn={!muted}
+          micOn={audioStarted}
         >
-          You
+          <div className="flex flex-col justify-between h-full p-2">
+            <p className="p-2">Your broadcast</p>
+            {callStarted && isConnected ? (
+              <div className="flex flex-row justify-center gap-4">
+                <button
+                  onClick={handleToggleAudio}
+                  title={audioStarted ? "Mute" : "Unmute"}
+                >
+                  <SecondaryButton>
+                    {audioStarted ? (
+                      <MuteIcon className="p-2" size="3em" />
+                    ) : (
+                      <UnmuteIcon className="p-2" size="3em" />
+                    )}
+                  </SecondaryButton>
+                </button>
+                <button
+                  onClick={handleToggleVideo}
+                  title={videoStarted ? "Turn video off" : "Turn video on"}
+                >
+                  <SecondaryButton>
+                    {videoStarted ? (
+                      <VideoOffIcon className="p-2" size="3em" />
+                    ) : (
+                      <VideoOnIcon className="p-2" size="3em" />
+                    )}
+                  </SecondaryButton>
+                </button>
+                <button onClick={handleEndCall} title="End stream">
+                  <PrimaryButton>
+                    <StreamOffIcon className="p-1" size="3em" />
+                  </PrimaryButton>
+                </button>
+              </div>
+            ) : callStarted ? (
+              <div>Loading...</div>
+            ) : (
+              <div className="text-center">
+                <button onClick={handleStarCall} title="Go live!">
+                  <PrimaryButton>
+                    <div className="flex flex-row items-center px-2 gap-2">
+                      <p>Go live!</p>
+                      <StreamOnIcon className="p-1" size="3em" />
+                    </div>
+                  </PrimaryButton>
+                </button>
+              </div>
+            )}
+          </div>
         </LocalUser>
       </div>
 
-      <button onClick={handleCall}>
-        <PrimaryButton>Call</PrimaryButton>
-      </button>
+      {otherHosts.map((host) => {
+        console.log(host);
+        return (
+          <div
+            key={host.uid}
+            className="rounded-3xl overflow-hidden bg-black w-1/2 h-80"
+          >
+            <RemoteUser user={host}>
+              <p className="p-4">{host.uid}</p>
+            </RemoteUser>
+          </div>
+        );
+      })}
     </div>
   );
 }
